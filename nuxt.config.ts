148a430fd965326f54bddf94cfd51db8560516b0
ixpl0 <kykymapua+github@gmail.com>
2026-02-27 02:52:57 +0400

    fix(content): use native sqlite connector for vercel SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,9 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   content: {
+    experimental: {
+      sqliteConnector: 'native',
+    },
     database: {
       type: 'sqlite',
       filename: isVercel ? '/tmp/contents.sqlite' : ':memory:',
