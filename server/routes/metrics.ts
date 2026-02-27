a97ecc98759f3ae3e20df419e9deadb47b33a380
Maksim Moiseev <mos.mix@gmail.com>
2026-02-27 11:34:56 +0300

    fix lint

--- server/routes/metrics.ts
+++ server/routes/metrics.ts
@@ -1,6 +1,6 @@
-import { getPortalMetrics, metricsContentType } from '../utils/metrics';
+import { getPortalMetrics, metricsContentType } from '../utils/metrics'
 
 export default defineEventHandler(async (event) => {
-  setHeader(event, 'content-type', metricsContentType());
-  return await getPortalMetrics();
-});
+  setHeader(event, 'content-type', metricsContentType())
+  return await getPortalMetrics()
+})
